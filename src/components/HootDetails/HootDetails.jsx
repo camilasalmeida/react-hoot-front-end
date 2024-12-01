4227bcf7b15167565aa86886398a3ffa5526be59
Camila Alm <camila.salmeidatech@hotmail.com>
2024-12-01 01:38:15 -0500

    add HootDetails styling

--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -1,20 +1,18 @@
 // src/components/HootDetails/HootDetails.jsx
 import { useParams } from "react-router-dom";
 import { useState, useEffect, useContext } from "react";
-import * as hootService from '../../services/hootService';
-import CommentForm from '../CommentForm/CommentForm';
+import * as hootService from "../../services/hootService";
+import CommentForm from "../CommentForm/CommentForm";
 import { AuthedUserContext } from "../../App";
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';  
-
-
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styles from "./HootDetails.module.css";
 
 const HootDetails = (props) => {
   const { hootId } = useParams(); // Make sure to destructure the hootId when calling upon useParams().
   console.log("hootId is:", hootId);
   const [hoot, setHoot] = useState(null);
   const user = useContext(AuthedUserContext);
-  
 
   useEffect(() => {
     const fetchHoot = async () => {
@@ -33,33 +31,41 @@ const HootDetails = (props) => {
   };
 
   const handleDeleteComment = async (commentId) => {
-    console.log('commentId is:', commentId);
+    console.log("commentId is:", commentId);
     await hootService.deleteComment(hootId, commentId);
     setHoot({
-        ...hoot,
-        comments: hoot.comments.filter((comment) => comment._id !== commentId)
+      ...hoot,
+      comments: hoot.comments.filter((comment) => comment._id !== commentId),
     });
-};
-
+  };
 
   //------------------------------------\\
 
-  if (!hoot) return <main>Loading...</main>
+  if (!hoot) return <main>Loading...</main>;
   return (
-    <main>
+    <main className={styles.container}>
       <header>
-      <p>{hoot?.category?.toUpperCase() || "No Category"}</p>
+        <p>{hoot?.category?.toUpperCase() || "No Category"}</p>
         <h1>{hoot.title}</h1>
-        <p>
-          {hoot.author.username} posted on
-          {new Date(hoot.createdAt).toLocaleDateString()}
-        </p>
-        {hoot.author._id === user._id && (
-          <>
-            <Link to={`/hoots/${hootId}/edit`}>Edit</Link>
-            <button onClick={() => {props.handleDeleteHoot(hootId)}}>Delete</button>
-          </>
-        )}
+
+        <div>
+          <p>
+            {hoot.author.username} posted on
+            {new Date(hoot.createdAt).toLocaleDateString()}
+          </p>
+          {hoot.author._id === user._id && (
+            <>
+              <Link to={`/hoots/${hootId}/edit`}>Edit</Link>
+              <button
+                onClick={() => {
+                  props.handleDeleteHoot(hootId);
+                }}
+              >
+                Delete
+              </button>
+            </>
+          )}
+        </div>
       </header>
 
       <p>{hoot.text}</p>
@@ -70,30 +76,31 @@ const HootDetails = (props) => {
 
         {!hoot.comments?.length && <p>There are no comments.</p>}
         {hoot.comments?.map((comment) => (
-            <article key={comment._id}> 
+          <article key={comment._id}>
             <header>
-              <p>
-                {comment.author.username} posted on
-                {new Date(comment.createdAt).toLocaleDateString()}
-              </p>
-              {comment.author._id === user._id && (
-                <>
-              <Link to={`/hoots/${hootId}/comments/${comment._id}/edit`}>Edit</Link>
-            <button onClick={() => handleDeleteComment(comment._id)}>Delete</button>
-            </>
-)}
+              <div>
+                <p>
+                  {comment.author.username} posted on
+                  {new Date(comment.createdAt).toLocaleDateString()}
+                </p>
+                {comment.author._id === user._id && (
+                  <>
+                    <Link to={`/hoots/${hootId}/comments/${comment._id}/edit`}>
+                      Edit
+                    </Link>
+                    <button onClick={() => handleDeleteComment(comment._id)}>
+                      Delete
+                    </button>
+                  </>
+                )}
+              </div>
             </header>
             <p>{comment.text}</p>
           </article>
-            ))}
+        ))}
       </section>
     </main>
   );
 };
 
-
 export default HootDetails;
-
-
-
-
